61407261c4e76c6cade1cc104b400b159a86abdd
Grace Chiang <gracechiang@me.com>
2018-03-06 20:49:05 +0000

    changed console output to beautified string

--- test_script.js
+++ test_script.js
@@ -14,13 +14,19 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
+  console.log("Searching ...")
 
 const queryString = "SELECT * FROM famous_people WHERE last_name = $1::text"
 client.query(queryString, [process.argv[2]], (err, result) => {
+  const id = result.rows[0].id;
+  const firstName = result.rows[0].first_name;
+  const lastName = result.rows[0].last_name;
+  const birthday = result.rows[0].birthdate;
   if (err) {
     return console.error("error running query", err);
   }
-  console.log(result.rows[0]); //output: 1
+  console.log("Found " + id + " person(s) by the name '" + lastName + "':");
+  console.log("-" + id + ": " + firstName + " " + lastName + ", born '" + birthday + "'");
   client.end();
   });
 });
