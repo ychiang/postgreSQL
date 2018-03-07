e2937145b4f5921d3746919d8c33a68b917c000b
Grace Chiang <gracechiang@me.com>
2018-03-07 03:11:25 +0000

    milestone table created with id

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
+});
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: client.host,
+      user: client.user,
+      password: client.password,
+      database: client.database
+    }
+  }
+
+};
